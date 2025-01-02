830c6f47375da318d352fd4aa0203f53e0a941c4
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-01-02 23:09:32 +0900

    自動通知のメンション対象ユーザーの削除コマンドを追加。

--- discord-commands/settings.js
+++ discord-commands/settings.js
@@ -23,6 +23,11 @@ export const SettingsCommand = {
 							.setName("add-role").setDescription("自動通知のメンション対象ロールを追加します。")
 							.addRoleOption(option => option.setName("role").setDescription("ロールを指定して下さい。").setRequired(true))
 					)
+					.addSubcommand(subcommand =>
+						subcommand
+							.setName("remove-user").setDescription("自動通知のメンション対象ユーザーを削除します。")
+							.addUserOption(option => option.setName("user").setDescription("ユーザーを指定して下さい。").setRequired(true))
+					)
 					.addSubcommand(subcommand => subcommand.setName("list").setDescription("自動通知のメンション対象の一覧を表示します。"))
 			),
 	execute:
@@ -126,6 +131,48 @@ export const SettingsCommand = {
 												MYSQL_CONNECTION.end();
 											}
 										});
+									}
+								}
+							});
+
+							break;
+
+						case "remove-user":
+							MYSQL_CONNECTION.query("SELECT mentions FROM discord_servers WHERE server_id=" + SERVER_ID + ";", (error, results) => {
+								if (error) {
+									console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss]"));
+									console.error(error);
+									MYSQL_CONNECTION.end();
+									return;
+								} else {
+									const USER_ID = interaction.options.getUser("user").id;
+									let data = JSON.parse(results[0]["mentions"]);
+
+									if (data && data.users) {
+										if (data.users.includes(USER_ID)) {
+											console.log(USER_ID);
+											console.log(data.users);
+											data.users.splice(data.users.findIndex(USER_ID), 1);
+
+											MYSQL_CONNECTION.query("UPDATE discord_servers SET mentions='" + JSON.stringify(data) + "' WHERE server_id=" + SERVER_ID + ";", (error, results) => {
+												if (error) {
+													console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss]"));
+													console.error(error);
+													MYSQL_CONNECTION.end();
+													return;
+												} else {
+													const SENDER = new Sender({ plain: "自動通知のメンション対象から <@" + USER_ID + "> を削除しました。" }, []);
+													SENDER.setDiscordOption();
+													SENDER.replyToDiscord(interaction);
+													MYSQL_CONNECTION.end();
+												}
+											});
+										} else {
+											const SENDER = new Sender({ plain: "自動通知のメンション対象に <@" + USER_ID + "> は登録されていません。" }, []);
+											SENDER.setDiscordOption();
+											SENDER.replyToDiscord(interaction);
+											MYSQL_CONNECTION.end();
+										}
 									}
 								}
 							});
