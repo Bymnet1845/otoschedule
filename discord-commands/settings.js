4991dcf5b5ce7c96e7b0136ab13d65c25fcb5ab5
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-01-03 00:47:26 +0900

    自動通知のメンション対象ロールを削除するコマンドを追加。

--- discord-commands/settings.js
+++ discord-commands/settings.js
@@ -28,6 +28,11 @@ export const SettingsCommand = {
 							.setName("remove-user").setDescription("自動通知のメンション対象ユーザーを削除します。")
 							.addUserOption(option => option.setName("user").setDescription("ユーザーを指定して下さい。").setRequired(true))
 					)
+					.addSubcommand(subcommand =>
+						subcommand
+							.setName("remove-role").setDescription("自動通知のメンション対象ロールを削除します。")
+							.addRoleOption(option => option.setName("role").setDescription("ロールを指定して下さい。").setRequired(true))
+					)
 					.addSubcommand(subcommand => subcommand.setName("list").setDescription("自動通知のメンション対象の一覧を表示します。"))
 			),
 	execute:
@@ -155,6 +160,44 @@ export const SettingsCommand = {
 												return;
 											} else {
 												const SENDER = new Sender({ plain: "自動通知のメンション対象から <@" + USER_ID + "> を削除しました。" }, []);
+												SENDER.setDiscordOption();
+												SENDER.replyToDiscord(interaction);
+												MYSQL_CONNECTION.end();
+											}
+										});
+									} else {
+										const SENDER = new Sender({ plain: "自動通知のメンション対象に <@" + USER_ID + "> は登録されていません。" }, []);
+										SENDER.setDiscordOption();
+										SENDER.replyToDiscord(interaction);
+										MYSQL_CONNECTION.end();
+									}
+								}
+							});
+
+							break;
+
+						case "remove-role":
+							MYSQL_CONNECTION.query("SELECT mentions FROM discord_servers WHERE server_id=" + SERVER_ID + ";", (error, results) => {
+								if (error) {
+									console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss]"));
+									console.error(error);
+									MYSQL_CONNECTION.end();
+									return;
+								} else {
+									const ROLE_ID = interaction.options.getRole("role").id;
+									let data = JSON.parse(results[0]["mentions"]);
+
+									if (data.roles.includes(ROLE_ID)) {
+										data.roles.splice(data.roles.findIndex((role) => { role === ROLE_ID }), 1);
+
+										MYSQL_CONNECTION.query("UPDATE discord_servers SET mentions='" + JSON.stringify(data) + "' WHERE server_id=" + SERVER_ID + ";", (error, results) => {
+											if (error) {
+												console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss]"));
+												console.error(error);
+												MYSQL_CONNECTION.end();
+												return;
+											} else {
+												const SENDER = new Sender({ plain: "自動通知のメンション対象から <@&" + ROLE_ID + "> を削除しました。" }, []);
 												SENDER.setDiscordOption();
 												SENDER.replyToDiscord(interaction);
 												MYSQL_CONNECTION.end();
