849927826be2402459775a9abfb80170110e7631
Bymnet1845 <haraheri5robymnet1845@yahoo.co.jp>
2025-01-26 21:13:26 +0900

    ログをコンマ秒に対応。

--- output-log.js
+++ output-log.js
@@ -1,7 +1,7 @@
 import { format } from "date-fns";
 
 export default function outputLog(text, type = "normal") {
-	console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss]"));
+	console.log(format(Date.now(), "[yyyy-MM-dd HH:mm:ss.SSS]"));
 
 	switch (type) {
 		case "error": console.error(text); break;
